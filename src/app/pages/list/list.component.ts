577d123fdbd583cc401ab3c7e0afec9e21ec23b8
José Antonio <jose.silva.br@outlook.com>
2024-03-16 13:48:50 -0300

    feat: recuperando lista a cada x tempo

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { VideoProcessModel } from '../../models/video-process.model';
 import { VideoProcessingService } from '../../services/video-processing.service';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
   selector: 'app-list',
@@ -13,18 +14,32 @@ import { VideoProcessingService } from '../../services/video-processing.service'
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, OnDestroy {
 
-  
   displayedColumns: string[] = ['id', 'inputName', 'outputName', 'status', 'actions'];
   dataSource = new MatTableDataSource<VideoProcessModel>();
 
+  private subscription: Subscription;
+  recoveryTime = 30000;
 
   constructor(private videoProcessingService: VideoProcessingService, private snackBar: MatSnackBar){
 
+    this.subscription = interval(this.recoveryTime).subscribe(() => {
+      this.load();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
   ngOnInit(): void {
+    this.load();
+  }
+
+  private load() {
     this.videoProcessingService.getAll()
     .subscribe({
       next: (data) => {
@@ -33,6 +48,6 @@ export class ListComponent implements OnInit {
       error: () => {
         this.snackBar.open('Houve um problema de comunicação', 'OK');
       }
-    })
+    });
   }
 }
